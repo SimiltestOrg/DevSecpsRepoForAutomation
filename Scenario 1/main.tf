bfbccf99cd93f8e2e7a54863e0620e44826b265e
simil.mittal <simil.mittal@algosec.com>
2022-10-03 14:19:11 +0530

    Checking scenario 7

--- Scenario 1/main.tf
+++ Scenario 1/main.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "Scenario1" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/33"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
