20367f9850d36c3390f3440292674fb3ff106b36
simil.mittal <simil.mittal@algosec.com>
2022-10-10 14:25:38 +0530

    main folder

--- Scenario 1/main.tf
+++ Scenario 1/main.tf
@@ -3,27 +3,19 @@ provider "aws" {
 }
 
 resource "aws_security_group" "Scenario1" {
-  name        = "SG with all port and  all IP."
-
-
-
-
-
-  vpc_id      = "VPC-481b202f"
+  name        = "SG with all port and all IP."
+  vpc_id      = "vpc-481b202f"
 
   ingress {
-    from_port                           = 0
-
-
-
+    from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-
-
-
   }
 
+  tags = {
+    Name = "Scenario1"
+  }
 }
 
 
